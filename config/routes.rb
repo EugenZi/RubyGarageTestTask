ddaba8a5793b17fb46a3beb5acc266974b558fa7
evgeny.zibrov <evgeny.zibrov@gmail.com>
2012-11-26 13:06:11 +0200

    Add device model User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 TaskList::Application.routes.draw do
+  devise_for :users
+
+  root :to => "index#index"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
