62392bbfe4b06e34ed8197551405deee7cad9d7a
evgeny.zibrov <evgeny.zibrov@gmail.com>
2012-12-18 12:43:32 +0200

    Update visual style

--- app/assets/javascripts/angular/controllers/projects.js
+++ app/assets/javascripts/angular/controllers/projects.js
@@ -1,18 +1,24 @@
 (function(global, document){
+
 	global.ProjectsController = function($scope, $http) {
+
 	  	var _this = this;
+
 	  	$http.get('/list.json').success(function(data) {
 	    	$scope.itemsList = data;
 	  	});
 
 	  	$('input.enabled').live("focusout", function(){
+
 	  		var form = $(this).closest("form"),
 	  			data = form.serialize(),
 	  			url  = form.attr("action"),
 	  			type = form.attr("method");
+
 	  		$(this).addClass('disabled')
 				   .removeClass('enabled')
 				   .attr('disabled','disabled');
+
 			$.ajax({
 				url:  url,
 				type: type,
@@ -21,16 +27,21 @@
 					$scope.itemsList = request_data;
 				}
 			});
+
 	    });
 
 	  	$('.edit_todo_list').live('submit', function(event){
+
 	  		$(this).find('input.enabled').trigger('focusout');
 	  		event.preventDefault(true);
+
 	  	});
 
 	  	$('.delete_empty_todo_list').live('click',function(event){
+
 	  		$(this).closest(".item").remove();
 			event.preventDefault(true);
+
 	  	});
 
 	  	$scope.addTodoList = function($event) {
@@ -38,6 +49,7 @@
 	    									  .insertAfter(".item:last")
 	    									  .removeAttr("id"),
 	    		form       = form_table.find('form');
+
 	    	form.live("submit", function(event){ 
 	    		var data = form.serialize(),
 	  			url  = form.attr("action"),
@@ -59,12 +71,20 @@
 	  	}
 
 	  	$scope.editTodoList = function($event) {
-	    	$($event.srcElement).closest('.block_top').find("input.disabled").removeClass('disabled').addClass('enabled').removeAttr('disabled');
+
+	    	$($event.srcElement).closest('.block_top')
+	    						.find("input.disabled")
+	    						.removeClass('disabled')
+	    						.addClass('enabled')
+	    						.removeAttr('disabled');
+
 	  		$event.preventDefault(true);
 	  	}
 
 		$scope.deleteTodoList = function($event) {
+
 			var elem = $($event.srcElement).parent("a");
+
 			$.ajax({
 				url:  elem.attr('href'),
 				success: function(){
@@ -73,7 +93,11 @@
 		  			});
 				}
 			});
+
 			$event.preventDefault(true);
+
 		}
+
 	};
+
 })(window, window.document)
--- app/assets/javascripts/angular/controllers/tasks.coffee
+++
@@ -1,3 +0,0 @@
-class @TasksController
-  @constructor:->
-    1
